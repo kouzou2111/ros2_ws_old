c9f8c6043eb017c50e4682ce15131b7984322236
kouzou2111 <byopo@bangban.uk>
2023-11-20 17:19:06 +0900

    add

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'mypkg'
 
@@ -10,13 +12,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='fass1080',
     maintainer_email='byopo@bangban.uk',
     description='TODO: Package description',
-    license='TODO: License declaration',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
